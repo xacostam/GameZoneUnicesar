baf509557fae72aa30ec44fadab185ca3b6d0558
xacosta <xacosta@unicesar.edu.co>
2026-09-08 08:10:21 -0500

    the class of customer was created

---
+++ src/main/java/com/gamezone/model/Customer.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gamezone.model;
+
+/**
+ *
+ * @author User
+ */
+public class Customer extends Person {
+    private String email;
+
+    public Customer(String name, String identification, String phone, String email) {
+        super(name, identification, phone);
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
