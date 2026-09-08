38f256564b800d13259d3990bcdb4405414d2dc4
xacosta <xacosta@unicesar.edu.co>
2026-09-08 08:17:37 -0500

    The class add product was created

--- src/main/java/com/gamezone/model/Sale.java
+++ src/main/java/com/gamezone/model/Sale.java
@@ -4,10 +4,42 @@
  */
 package com.gamezone.model;
 
-/**
- *
- * @author User
- */
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sale {
+    private LocalDate date;
+    private Customer customer;
+    private Seller seller;
+    private List<Product> products;
+
+    public Sale(LocalDate date, Customer customer, Seller seller) {
+        this.date = date;
+        this.customer = customer;
+        this.seller = seller;
+        this.products = new ArrayList<>();
+    }
     
+    public void addProduct(Product product) {
+        if (product == null) {
+            throw new IllegalArgumentException("Product cannot be null.");
+        }
+
+        products.add(product);
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
 }
+
